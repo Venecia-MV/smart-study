282cb0a4837890bf0d65d8e600771cfcadb2d9d0
Michael Jacinto Venecia <mjveneci+ncstate@ncsu.edu>
2026-02-06 20:52:04 -0500

    Initial project setup with Spring Boot and SQLite

---
+++ smart-study/src/test/java/com/venecia/smart_study/SmartStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venecia.smart_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
